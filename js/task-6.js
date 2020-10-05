cbfca989ba0ac13cd51cf26b7d7a9a6b273e1043
Alexandra-Pedan <sashka28465@gmail.com>
2020-10-05 22:00:47 +0300

    task6-7

--- js/task-6.js
+++ js/task-6.js
@@ -23,8 +23,15 @@
 // }
 
 const input = document.querySelector("#validation-input");
+const inputLength = input.getAttribute("data-length");
 input.addEventListener("blur", onInputBlur);
 
 function onInputBlur() {
-  console.log("Инпут потерял фокус - событие blur");
+  if (input.value.length > inputLength) {
+    input.classList.add("valid");
+    input.classList.remove("invalid");
+  } else {
+    input.classList.add("invalid");
+    input.classList.remove("valid");
+  }
 }
