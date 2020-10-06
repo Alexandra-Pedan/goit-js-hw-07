350fd75566bb461c4654f8ef55ff1528a68f5ae7
Alexandra-Pedan <sashka28465@gmail.com>
2020-10-06 17:13:34 +0300

    hw7

--- js/task-3.js
+++ js/task-3.js
@@ -25,38 +25,33 @@
 //   },
 // ];
 
-
 const images = [
-    {
-      url:
-        'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-      alt: 'White and Black Long Fur Cat',
-    },
-    {
-      url:
-        'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-      alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-    },
-    {
-      url:
-        'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-      alt: 'Group of Horses Running',
-    },
-  ];
+  {
+    url:
+      "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url:
+      "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url:
+      "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+];
 
+const galeryEl = document.querySelector("#gallery");
 
+galeryEl.insertAdjacentHTML("afterbegin", galleryImg());
 
-  const galeryEl = document.querySelector('#gallery');
-
-  galeryEl.insertAdjacentHTML('afterbegin', images.reduce((acc, {url, alt}) => {
-    acc += `<li> <img class = "gallery_image" src="${url}" alt="${alt}"/> </li>`;
-    return acc
-  })
-  );
- 
-
-
-  console.log(galeryEl);
-
-
-
+function galleryImg() {
+  return images
+    .map(
+      ({ url, alt }) =>
+        `<li class="gallery_li" ><img class="gallery_image" src="${url}" alt="${alt}"></li>`
+    )
+    .join("");
+}
